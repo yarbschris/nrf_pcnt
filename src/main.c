556b56ca96dc1d7c17096e4ec2deacf4b67e44b5
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-12-15 12:54:46 -0600

    Increment counter via ppi, now trigger through GPIOTE

--- src/main.c
+++ src/main.c
@@ -1,12 +1,27 @@
 #include <nrfx_timer.h>
+#include <nrfx_ppi.h>
 #include <zephyr/kernel.h>
 
 #define TIMER_NUM 0 // Change this if application already uses default timer (0)
 
 static const nrfx_timer_t pcntTimer = NRFX_TIMER_INSTANCE(TIMER_NUM);
 
+// FOR PPI TESTING
+
+static const nrfx_timer_t cntTimer = NRFX_TIMER_INSTANCE(1);
+void setupCntTimer() {
+        nrfx_timer_config_t cntTimerConfig = NRFX_TIMER_DEFAULT_CONFIG(1000000);
+        uint32_t err = nrfx_timer_init(&cntTimer, &cntTimerConfig, NULL);
+        if (err != 0) {
+                printk("nrfx_timer_init error: %08x", err);
+                return;
+        }
+        uint32_t timer_ticks = nrfx_timer_us_to_ticks(&cntTimer, 1);
+        nrfx_timer_extended_compare(&cntTimer, NRF_TIMER_CC_CHANNEL1, timer_ticks, NRF_TIMER_SHORT_COMPARE1_CLEAR_MASK, false);
+}
+
 void setupTimer() {
-  nrfx_timer_config_t pcntTimerConfig = NRFX_TIMER_DEFAULT_CONFIG(1000000);
+  nrfx_timer_config_t pcntTimerConfig = NRFX_TIMER_DEFAULT_CONFIG(9000000);
   pcntTimerConfig.mode = NRF_TIMER_MODE_COUNTER;
 
   nrfx_err_t errCode = nrfx_timer_init(&pcntTimer, &pcntTimerConfig, NULL);
@@ -20,13 +35,7 @@ void setupTimer() {
   printk("timer enabled\n");
 }
 
-// Will be replaced with a timer COUNT event triggered by gpio pin further along
-// This function is for test purposes
-void incrementCounter() {
-  nrfx_timer_increment(&pcntTimer);
-}
-
-uint32_t clearCounter() {
+void clearCounter() {
   printk("Clearing pcntTimer\n");
   nrfx_timer_clear(&pcntTimer);
 }
@@ -45,11 +54,26 @@ uint32_t getCounterCountAddress() {
         return addr;
 }
 
+// PPI
+static nrf_ppi_channel_t ppiChannel;
+void setupPPI() {
+        nrfx_ppi_channel_alloc(&ppiChannel);
+
+        nrfx_ppi_channel_assign(ppiChannel,
+                nrfx_timer_compare_event_address_get(&cntTimer, NRF_TIMER_CC_CHANNEL1),
+                nrfx_timer_task_address_get(&pcntTimer, NRF_TIMER_TASK_COUNT));
+
+        nrfx_ppi_channel_enable(ppiChannel);
+}
+
 int main(void) {
-  printk("Starting...\n");
-  k_msleep(100);
   setupTimer();
+  setupCntTimer();
+  setupPPI();
+  nrfx_timer_enable(&cntTimer);
   printk("Finished Setup\n");
-  getCounterCountAddress();
+  while(1) {
+        getCounterVal();
+  }
   return 0;
 }
