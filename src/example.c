249d9061413e7f849ba0fe70b00a80324a7e3418
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-12-16 18:15:24 -0600

    main renamed to example

--- src/example.c
+++ src/example.c
@@ -10,7 +10,7 @@ int main() {
 
         for (;;) {
                 pcntClear();
-                printk("Sleeping for %d seconds, peripherals will still count pulses even while CPU is asleep\n", SLEEP_TIME_MS / 1000);
+                printk("Sleeping for %d seconds, peripherals will still count pulses even while CPU is asleep\n\n", SLEEP_TIME_MS / 1000);
                 k_msleep(SLEEP_TIME_MS);
                 count = pcntGetCount();
                 printk("%u pulse(s) counted this cycle, resetting counter...\n\n", count);
